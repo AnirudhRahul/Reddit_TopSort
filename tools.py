fd534c36e1e9eb5b088612a522428c2acadb7646
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-05-25 03:28:49 -0400

    Created options to work with all/sfw/nsfw subreddits

--- tools.py
+++ tools.py
@@ -1,11 +1,34 @@
-def subreddit_map():
+def all_subreddit_map():
     f = open("topSubreddits.txt", "r")
     data = dict()
     index = 0
     for line in f:
         vals = line.split()
-        data[vals[0]]=[index,int(vals[1])]
+        data[vals[0]]=[index,int(vals[2])]
         index+=1
     return data
 
-print(subreddit_map())
+def sfw_subreddit_map():
+    f = open("topSubreddits.txt", "r")
+    data = dict()
+    index = 0
+    for line in f:
+        vals = line.split()
+        if vals[1]=='sfw':
+            data[vals[0]]=[index,int(vals[2])]
+            index+=1
+    return data
+
+def nsfw_subreddit_map():
+    f = open("topSubreddits.txt", "r")
+    data = dict()
+    index = 0
+    for line in f:
+        vals = line.split()
+        if vals[1]=='nsfw':
+            data[vals[0]]=[index,int(vals[2])]
+            index+=1
+    return data
+
+print(sfw_subreddit_map())
+print(len(sfw_subreddit_map()))
