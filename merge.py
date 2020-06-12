20de6b8cdff4cd545bca22737367cbca7b61b1e7
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-06-11 20:39:44 -0400

    Created merge function to merge output from different comment_files

---
+++ merge.py
@@ -0,0 +1,26 @@
+import sys
+import tools
+import os
+from os.path import join
+import glob
+
+def mergeFiles(outputDir):
+    output_folders = filter(os.path.isdir, glob.glob(join(outputDir,'*')))
+    output_folders = sorted(list(output_folders))
+    size = len(output_folders)
+    i = 1
+    for folder in output_folders:
+      whole_list = set()
+      files = filter(lambda x:not x.startswith('completed'),os.listdir(folder))
+      files = list(files)
+      if len(files)>1:
+          for doc in files:
+            path = join(folder,doc)
+            new_path = join(folder,'completed_'+doc)
+            whole_list.update(tools.fileToList(path))
+            if doc !='combined.hex':
+                os.rename(path,new_path)
+          tools.listToFile(sorted(list(whole_list)), join(folder,'combined.hex'))
+
+      print('\r',i,'/',size,sep='',end='')
+      i+=1
