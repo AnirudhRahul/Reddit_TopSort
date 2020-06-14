e62d9a34be1929c27f96e4a0f14ee62e7e68132a
AnirudhRahul <anirudhrahul@anjurahul.com>
2020-06-14 17:19:48 -0400

    Created program to migrate output from one output folder to another

---
+++ transplant.py
@@ -0,0 +1,59 @@
+#Copy output from one dir to another
+import argparse
+import os
+import glob
+from os.path import join
+from distutils.dir_util import copy_tree
+
+def copyOver(toDir='',fromDir=''):
+    if not (toDir and fromDir):
+        raise NameError('Both arguements(toDir and fromDir) are required')
+    print("Retrieving files:",fromDir)
+    fromFiles = getFiles(fromDir)
+    print("Retrieving files:",toDir)
+    toFiles = getFiles(toDir)
+    for file in toFiles:
+        found = [item for item in fromFiles if item[-1] == file[-1]]
+        if found:
+            # print('From',found[0][0])
+            # print('To',file)
+            copy_tree(found[0][0],file[0])
+        else:
+            print('No output found for',file[2],file[1],found)
+
+    # print(getFiles(toDir))
+    # print(getFiles(fromDir))
+
+
+def getFiles(outputDir):
+    output_folders = filter(os.path.isdir, glob.glob(join(outputDir,'*')))
+    output_folders = list(output_folders)
+    size = len(output_folders)
+    i = 1
+    out_list = []
+    for folder in output_folders:
+      out_list.append((folder,*os.path.basename(folder).split('-')))
+      print('\r',i,'/',size,sep='',end='')
+      i+=1
+    print()
+    return out_list
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description='Use this program to transplant output from one folder to another',
+    )
+    parser.add_argument('ToDir',
+                        metavar='todir',
+                        nargs='?',
+                        type=str,
+                        default='',
+                        help='Output folder to import from')
+    parser.add_argument('FromDir',
+                    metavar='fromdir',
+                    nargs='?',
+                    type=str,
+                    default='',
+                    help='Output folder to export into')
+
+    args = parser.parse_args()
+    copyOver(toDir=args.ToDir,fromDir=args.FromDir)
